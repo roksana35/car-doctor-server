e3bcdc3d9f5881fa61815bcaddfca7e5d5761e92
roksana35 <roksana7235@gmail.com>
2024-05-08 19:11:26 +0600

    add all file

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
 const express = require('express');
 const cors = require('cors');
+const jwt =require('jsonwebtoken')
+const cookieparser=require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin:[
+        'http://localhost:5173',
+        'https://car-doctor-crud-39fae.web.app',
+        'https://car-doctor-crud-39fae.firebaseapp.com'
+    ],
+    credentials:true
+}));
 app.use(express.json());
+app.use(cookieparser())
+
+
 
 
 console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmgfwvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+
+
 });
+const logger=(req,res,next)=>{
+    console.log('log info',req.method,req.url)
+    next()
+}
+    
+
+const verifytoken=(req,res,next)=>{
+    const token=req.cookies.token;
+    console.log('token in the middlewar',token)
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(!token){
+            return res.status(401).send({message:'unauthorize access'})
+        }
+        req.user=decoded
+        next()
+    })
+  
+}
+
+
+const cookieOptions={
+    httpOnly:true,
+                secure:process.env.NODE_ENV ==='production'?true:false,
+                sameSite:process.env.NODE_ENV ==='production'?'none':'strict',
+
+}
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
 
-        const serviceCollection = client.db('carDoctor').collection('services');
-        const bookingCollection = client.db('carDoctor').collection('bookings');
+    const serviceCollection = client.db('car-doctor').collection('services');
+        const bookingCollection = client.db('car-doctor').collection('bookings');
+
+
+        // auth related api
+        app.post('/jwt',async(req,res)=>{
+            const user=req.body;
+            console.log('user for token',user)
+            const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            res.cookie('token',token,cookieOptions)
+            .send({success:true})
+        })
+
+        app.post('/logout',async(req,res)=>{
+            const user = req.body;
+            console.log('logout user',user)
+            res.clearCookie('token',{...cookieOptions,maxAge:0}).send({success:true})
+        })
 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
@@ -52,11 +111,15 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings',logger,verifytoken, async (req, res) => {
             console.log(req.query.email);
+            
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
+            }
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message:'forbiden access'})
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
@@ -91,15 +154,25 @@ async function run() {
         })
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   
+  }
 }
 run().catch(console.dir);
+
+
+
+
+
+
+// akbeNQZFDw2ikFhI
+// carDB
+
+
 
 
 
